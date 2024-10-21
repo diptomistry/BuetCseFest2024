73f1fecf6a7cdd677abd98e207e36248d658219e
diptomistry <diptomistrybd@gmail.com>
2024-10-22 02:34:03 +0600

    Refactor SidebarDemo component to update label and height styles

--- frontend/components/dashboard/SidebarDemo.tsx
+++ frontend/components/dashboard/SidebarDemo.tsx
@@ -15,7 +15,7 @@ import { cn } from "@/lib/utils";
 export function SidebarDemo() {
   const links = [
     {
-      label: "Dashboard",
+      label: "Chatbot",
       href: "#",
       icon: (
         <IconBrandTabler className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
@@ -48,7 +48,7 @@ export function SidebarDemo() {
     <div
       className={cn(
         " flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1  border border-neutral-200 dark:border-neutral-700 overflow-hidden",
-        "h-[100vh]" // for your use case, use `h-screen` instead of `h-[60vh]`
+        "h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`
       )}
     >
       <Sidebar open={open} setOpen={setOpen}>
@@ -117,7 +117,7 @@ const Dashboard = () => {
   return (
     <div className="flex flex-1">
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-        <div className="flex gap-2">
+        <div className="flex flex-row gap-2">
           {[...new Array(4)].map((i) => (
             <div
               key={"first-array" + i}

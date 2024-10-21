4c550dfefb1dd410f3703775794db5d4f1e28cd2
diptomistry <diptomistrybd@gmail.com>
2024-10-22 02:06:53 +0600

    Refactor SidebarDemo component to fix logout href

--- frontend/components/dashboard/SidebarDemo.tsx
+++ frontend/components/dashboard/SidebarDemo.tsx
@@ -37,7 +37,7 @@ export function SidebarDemo() {
     },
     {
       label: "Logout",
-      href: "#",
+      href: "/",
       icon: (
         <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
